52dd25a16f92207ce53866a87014080f883749a8
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-01 16:32:56 +0200

    Open the page with set game

---
+++ index.js
@@ -0,0 +1,10 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+driver.get('http://www.setgame.com/set/puzzle');
+driver.quit();
