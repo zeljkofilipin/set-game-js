6da4bff6da1af78901c6faea6100a49a3ad4a25a
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-01 17:31:44 +0200

    The first test using mocha+chai

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var set = require('../lib/set');
+
+describe('Set game', function() {
+
+  describe('#set()', function () {
+
+    it('should return true when three cards are a set', function () {
+      var cards = [
+        "oval open green 2",
+        "oval open red 2",
+        "oval open blue 2"
+      ]
+      assert.isTrue(set(cards));
+    });
+
+  });
+
+});
