02d193f2a082ec65330ef234cb47e9421fd855b8
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-02 13:07:19 +0200

    test if a feature of 3 cards is the same

--- test/set.js
+++ test/set.js
@@ -1,5 +1,5 @@
-var assert = require('chai').assert;
-var set = require('../lib/set');
+const assert = require('chai').assert;
+const set = require('../lib/set');
 
 describe('Set game', function() {
 
@@ -11,7 +11,7 @@ describe('Set game', function() {
         "oval open red 2",
         "oval open blue 2"
       ];
-      assert.isTrue(set(cards));
+      assert.isTrue(set.set(cards));
     });
 
     it('should return false when three cards are not a set');/*, function () {
@@ -22,6 +22,19 @@ describe('Set game', function() {
       ];
       assert.isNotTrue(set(cards));
     });*/
+
+  });
+
+  describe('#feature()', function () {
+
+    it('should return true when a feature of three cards is the same', function () {
+      var features = [
+        "oval",
+        "oval",
+        "oval"
+      ];
+      assert.isTrue(set.feature(features));
+    });
 
   });
 
