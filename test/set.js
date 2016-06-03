682837852ff80cd1aec7016983fbe9a3eed3261c
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-03 12:47:13 +0200

    replaced "three cards" with just "cards"
    Since there are always three cards.

--- test/set.js
+++ test/set.js
@@ -5,7 +5,7 @@ describe('Set game', function () {
 
   describe('#set()', function () {
 
-    it('should return true when three cards are a set', function () {
+    it('should return true when cards are a set', function () {
       var cards = [
         'oval open green 2',
         'oval open red 2',
@@ -14,7 +14,7 @@ describe('Set game', function () {
       assert.isTrue(set.set(cards));
     });
 
-    it('should return false when three cards are not a set');/*, function () {
+    it('should return false when cards are not a set');/*, function () {
       var cards = [
         "squiggle solid red 1",
         "squiggle striped blue 1",
@@ -27,7 +27,7 @@ describe('Set game', function () {
 
   describe('#featureIsTheSame()', function () {
 
-    it('should return true when a feature of three cards is the same', function () {
+    it('should return true when a feature of cards is the same', function () {
       var features = [
         'oval',
         'oval',
@@ -36,7 +36,7 @@ describe('Set game', function () {
       assert.isTrue(set.featureIsTheSame(features));
     });
 
-    it('should return false when a feature of three cards is not the same', function () {
+    it('should return false when a feature of cards is not the same', function () {
       var features = [
         'squiggle',
         'oval',
