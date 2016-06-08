d76a12896592542a5ca54f662eb1075b4c06a9dd
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-08 10:55:48 +0200

    get table HTML from the page

--- test/selenium.js
+++ test/selenium.js
@@ -1,3 +1,4 @@
+const assert = require('chai').assert;
 var By = require('selenium-webdriver').By;
 var until = require('selenium-webdriver').until;
 var firefox = require('selenium-webdriver/firefox');
@@ -14,9 +15,16 @@ test.describe('Set game', function () {
     driver.quit();
   });
 
-  test.it('should open set game', function () {
+  test.it('should open the game', function () {
     driver.get('http://smart-games.org/en/set/start');
     driver.wait(until.titleIs('Set - online card game'), 1000);
   });
 
+  test.it('should get table HTML from the page', function () {
+    driver.get('http://smart-games.org/en/set/start');
+    driver.wait(until.titleIs('Set - online card game'), 1000);
+    driver.findElement(By.id('board')).getOuterHtml().then(html => {
+      assert.include(html, '<table id="board" class="" border="0">');
+    });
+  });
 });
